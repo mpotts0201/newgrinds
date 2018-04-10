d0c1fe4019444f61db6ef723f031d325d8b4bf1b
mpotts0201 <mpotts0201@gmail.com>
2018-04-10 12:46:04 -0400

    added index and show methods for coffee shops

--- app/controllers/api/coffee_shops_controller.rb
+++ app/controllers/api/coffee_shops_controller.rb
@@ -1,2 +1,17 @@
 class Api::CoffeeShopsController < ApplicationController
+    def index
+        @coffee_shops = CoffeeShop.all 
+        render json: {
+            coffee_shops: @coffee_shops
+        }
+    end
+
+    def show 
+        @coffee_shop = CoffeeShop.find(params[:id])
+        render json: {
+            coffee_shop: @coffee_shop
+        }
+    end
+
+
 end
