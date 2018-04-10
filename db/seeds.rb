5479593d279b7f870ea7e3f4dc07dba6fa00adf5
mpotts0201 <mpotts0201@gmail.com>
2018-04-10 12:41:44 -0400

    Seeded test coffee shop into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+CoffeeShop.destroy_all
 
 murphy = User.create(name: "Murphy Potts", aboutMe: "I made this app!")
 neil = User.create(name: "Neil Rajendran", aboutMe: "I enjoy a good cold brew, in the cold breeze.")
 cameron = User.create(name: "Cameron Gunter", aboutMe: "Slam, Bam, call me Cam.")
+
+zombie = CoffeeShop.create(name: "Zombie Coffee and Donuts", address: "350 Broad Street, Athens, GA", hours: "7:00 am - 10:00 pm Monday-Saturday, 8:00 am - 9:00 pm Sunday")
