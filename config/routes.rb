27ba3679ebdb2b3ed8fbb4a5ed0343537ca0ca76
mpotts0201 <mpotts0201@gmail.com>
2018-04-10 10:36:21 -0400

    created user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'users/index'
+  end
+
+  namespace :api do
+    get 'users/show'
+  end
+
+  namespace :api do
+    get 'users/create'
+  end
+
+  namespace :api do
+    get 'users/delete'
+  end
+
+  namespace :api do
+    get 'users/update'
+  end
+
+  namespace :api do
     resources :coffee
   end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409175217) do
+ActiveRecord::Schema.define(version: 20180410143321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "aboutMe"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
